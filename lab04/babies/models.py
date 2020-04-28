6fcfc4c61c2d80b3067f6f01057719a54dbcbcf5
Darlight <andres.perdomo09@gmail.com>
2020-04-27 18:20:00 -0600

    Creating primary models and modyfing settings

--- lab04/babies/models.py
+++ lab04/babies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Baby(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    parent = models.ForeignKey(
+        'parents.Parent',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return 'Baby: {}'.format(self.name)
--- lab04/events/models.py
+++ lab04/events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Event(models.Model):
+    event_type = models.CharField(max_length=80, null=True)
+    fecha = models.DateTimeField(default=datetime.now, blank=True)
+    note = models.CharField(max_length=800)
+    baby = models.ForeignKey(
+        'babies.Baby',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return 'Event: {}'.format(self.name)
